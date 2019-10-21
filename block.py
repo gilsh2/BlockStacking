199515e5e56ab2f3f6a92bce06c7b01483b33e31
Gil Lapid <gilsh@microsoft.com>
2019-10-20 18:19:19 -0700

    initial version

---
+++ block.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 19 15:55:53 2019
+
+@author: Alon Lapid
+"""
+
+import sys
+import re
+
+def custom_compare(a, b):
+    return a.x * a.y  - b.x * b.y
+
+def base(a):
+    return a.x * a.y
+
+#Class the hold the box demintions
+class Box:   
+    def __init__(self, x, y, z):     
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    def __str__(self):
+        return "(" + str(self.x) +"," +str(self.y) +"," +str(self.z) +")" 
+    
+    def __eq__(self, other): 
+        if(self.x == other.x and  self.y == other.y and self.z == other.z)  : 
+            return True
+        else: 
+            return False
+
+def GetNumberOfBlockInSolution(optimal,optimalHeight):
+    blocks = 0
+    startblock = optimal.index(optimalHeight);
+    next = startblock
+    while (True):                 
+        blocks = blocks +1
+        next = stacking[next]
+        if(next == 0 or next == None):
+            break
+        
+    return  blocks   
+    
+
+#Initialization 
+inputfile = ""
+outputfile =""
+if(len(sys.argv) > 2):
+    inputfile = sys.argv [1]
+    outputfile = sys.argv [2]
+else :
+    inputfile = "in.txt"
+    outputfile = "out.txt"
+
+# Reading input data     
+inh = open(inputfile)
+numberoflines = int(inh.readline());
+Boxes = [] 
+for i in range(1,numberoflines+1):
+    line = inh.readline().strip();
+    line = line.replace(",","")
+    l = list(map(int, re.split('\s+', line)))    
+    x = l[0]
+    y= l[1]
+    z = l[2]     
+    #Insert all 3 rotation of the box type
+    Boxes.append( Box(max(x,y),min(x,y),z))
+    Boxes.append(Box(max(z,x),min(z,x),y))
+    Boxes.append(Box(max(z,y),min(z,y),x))
+
+# Sorting boxes according to base area     
+Boxes = sorted(Boxes, key=base, reverse=True )    
+    
+
+optHeightArray  = [None] * (len(Boxes)+1)
+optHeightArray[0] =0
+stacking  = [None] * (len(Boxes)+1)
+
+# Computing all the solutions from i to n in which the ith block is ontop using the recursion nature of the problem
+# The i+1 solution evalutes all 1..i previous solution(stack of blocks) and find the best one to utilize ( or non at all) to place the i+1
+#box ontop of it.
+for i in range(1,len(Boxes)+1):
+    maxHeightIndex = 0
+    for j in reversed(range(0,i) ):        
+        if(Boxes[j].x > Boxes[i-1].x and  Boxes[j].y > Boxes[i-1].y   ):
+            if(optHeightArray[maxHeightIndex] < optHeightArray[j+1]):
+                        maxHeightIndex = j+1                          
+                        #print("set maxHeightIndex as  " + str(Boxes[j]) + ">" + str(Boxes[i-1])  ,i,j)                
+                  
+    optHeightArray[i]=optHeightArray[maxHeightIndex] + Boxes[i-1].z;    
+    stacking[i] = maxHeightIndex;
+    #print("optHeight for i=" + str(i)+" Boxes[i-1].z = " +str(Boxes[i-1].z) +" optHeight[maxHeightIndex]=" +str(optHeight[maxHeightIndex])  ,optHeight)
+    #print("stacking=",stacking)
+
+# getting the maximum stack out of all the stacks we computed 
+optimalHeight = max(optHeightArray);
+
+
+# calculating the number of blocka
+blocks = GetNumberOfBlockInSolution(optHeightArray,optimalHeight)
+print("The tallest tower has " + str(blocks) + " blocks and a height of " +str(optimalHeight))
+
+#write the output
+outh = open(outputfile, "w")
+outh.write(str(blocks) + "\n" ) 
+for i in range(0,blocks):
+    s = str(Boxes[i].x)  + " " +  str(Boxes[i].y) + " " + str(Boxes[i].z) 
+    outh.write(s+ "\n")
+    if(len(sys.argv) < 2):
+         print(s)
+    
+outh.flush()
+outh.close()  
+    
